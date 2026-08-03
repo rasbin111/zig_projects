90f4c6cdd76b995e7a225559e99c749813378060
rasbin <rasbingodarthapa@gmail.com>
2026-08-03 15:52:47 +0545

    complete prime number detection system

--- prime_number_detection/src/main.zig
+++ prime_number_detection/src/main.zig
@@ -3,14 +3,70 @@ const Io = std.Io;
 
 const io_utils = @import("io_utils.zig");
 
+pub fn detect_prime(num: u32) bool {
+    var isPrime = true;
+    for (2..num) |i| {
+        if (num % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    return isPrime;
+}
+
 pub fn main(init: std.process.Init) !void {
     var stdin_reader: io_utils.StdinReader = .{};
     var stdout_writer: io_utils.StdoutWriter = .{};
 
     const reader = stdin_reader.init(init.io);
-    _ = reader;
     const writer = stdout_writer.init(init.io);
 
     try writer.print("**Welcome to Prime Number Detection System** \n\n", .{});
     try writer.flush();
+
+    var num_str: []u8 = undefined;
+    var num: u32 = undefined;
+
+    while (true) {
+        try writer.print("Please enter a number: ", .{});
+        try writer.flush();
+
+        num_str = try reader.takeDelimiterExclusive('\n');
+        reader.toss(1);
+
+        try writer.print("\n", .{});
+        try writer.flush();
+
+        num = try std.fmt.parseInt(u32, num_str, 10);
+
+        const isPrime = detect_prime(num);
+
+        if (isPrime) {
+            try writer.print("Is a prime number\n", .{});
+            try writer.flush();
+        } else {
+            try writer.print("Not a prime number\n\n", .{});
+            try writer.flush();
+        }
+
+        var pdChar: []u8 = undefined;
+
+        while (true) {
+            try writer.print("Do you want to continue(y/n): ", .{});
+            try writer.flush();
+
+            pdChar = try reader.takeDelimiterExclusive('\n');
+            reader.toss(1);
+
+            if (std.mem.eql(u8, pdChar, "y") or std.mem.eql(u8, pdChar, "Y") or std.mem.eql(u8, pdChar, "n") or std.mem.eql(u8, pdChar, "N")) {
+                break;
+            }
+        }
+
+        if (std.mem.eql(u8, pdChar, "y") or std.mem.eql(u8, pdChar, "Y")) {
+            continue;
+        } else {
+            break;
+        }
+    }
 }
